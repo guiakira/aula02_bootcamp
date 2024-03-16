17115b00b06bcc6767910546a77b3c0c25329bd3
Guilherme Akira <akira.enoki@gmail.com>
2024-03-16 19:26:11 -0300

    Adicionando conteúdo de if, elif, else

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #Aula de Try,Except,else,Finally
 
-try:
-    resultado = len(3)
-    print(resultado)
-except TypeError as e:
-    print(e)
+# try:
+#     resultado = len("Luciano")
+#     print(resultado)
+# except TypeError as e:
+#     print(e) #Imprime o erro
+# else:
+#     print("Deu tudo certo") #No caso do try, quando dá certo, ele printa o else
+# finally:
+#     print("O importante é participar") #Printa independente da resposta
+
+
+# Casos com if:
+# numero = int(input("Insira um numero: "))
+# if isinstance(numero, int):
+#     print("a variavel é inteiro") #Imprime o erro
+# else:
+#     print("a variavel não é um inteiro") #No caso do try, quando dá certo, ele printa o else
+
+idade = 18
+
+if idade < 18:
+    print("Não pode dirigir")
+elif idade == 18:
+    print("Pode tirar a carteira de motorista")
+else:
+    print("Pode dirigir")

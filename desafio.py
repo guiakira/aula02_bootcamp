b834d783b5e7db8e47ff22ec7c924e731a563644
Guilherme Akira <akira.enoki@gmail.com>
2024-03-16 19:38:57 -0300

    Adicionando file de desafio e resolvendo o conversor de temperatura

---
+++ desafio.py
@@ -0,0 +1,14 @@
+### Desafios de Try, Except e if:
+#1. Conversor de temperatura
+#2. Verificador de palíndromo
+#3. Calculadora simples
+#4. Classificador de números:
+#5. Conversor de tipo com validação
+
+#1. Conversor de temperatura
+escala_temp =input("Qual escala você gostaria de usar: celsius, kelvin ou fahrenheit: ").lower()
+temperatura = float(input(f"Insira a temperatura em {escala_temp}: "))
+
+
+
+

eb79fa750776d13d6be91c122226d3f20c0c7634
Guilherme Akira <akira.enoki@gmail.com>
2024-03-16 18:51:52 -0300

    Resolvendo exercicio 2

--- exercicios.py
+++ exercicios.py
@@ -4,3 +4,10 @@ num_1 = int(input("Insira um número: "))
 num_2 = int(input("Insira outro número: "))
 resultado = num_1 // num_2
 print(resultado)
+
+
+#Ex. Escreva um programa que calcule a área de um círculo, recebeno o raio como entrada
+
+raio = float(input("Insira o raio: "))
+area = 3.14*raio*raio
+print(area)
